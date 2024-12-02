b46bb247a027f3c82bc30be24939c43b2eba91ab
Christine Hsieh(謝子涔) <Christine_Hsieh@asus.com>
2024-12-02 15:51:14 +0800

    add: ddrm

--- eval_fid.sh
+++ eval_fid.sh
@@ -1,2 +1,2 @@
-python eval.py --run_id SAC_v1 --save_path model/sample_model/411 --DM_model model/ddpm_ema_cifar10 --target_steps 10 --img_save_path run/sample/SAC_v1
+python eval.py --run_id SAC_v1 --save_path model/sample_model/369 --DM_model model/ddpm_ema_cifar10 --target_steps 10 --img_save_path run/sample/SAC_v1
 python fid_score.py run/sample/SAC_v1 run/fid_stats_cifar10.npz --device cuda:0 --batch-size 256
---
+++ test.py
@@ -0,0 +1,3 @@
+import torch
+print(torch.__version__)  # 檢查 PyTorch 版本
+print(torch.cuda.is_available())  # 檢查是否支援 CUDA
