bde071000750214071ac4bed58aa2b0b5425d62e
yammy0623 <83154615+yammy0623@users.noreply.github.com>
2024-12-13 15:41:58 +0800

    refactor: ddrm 2agent

--- envs/env.py
+++ envs/env.py
@@ -12,7 +12,8 @@ import random
 
 # DDRM
 from ddrm.datasets import get_dataset, data_transform, inverse_data_transform
-from ddrm.functions.denoising import initialize_generalized_steps, denoise_single_step
+from ddrm.functions.denoising import initialize_generalized_steps, denoise_single_step, denoise_guided_addnoise
+from skimage.metrics import structural_similarity
 import pdb 
 
 class DiffusionEnv(gym.Env):
@@ -22,7 +23,7 @@ class DiffusionEnv(gym.Env):
         # RL Setting
         self.agent1 = agent1 # RL model from subtask 1
         self.target_steps = target_steps
-        self.uniform_steps = [i for i in range(0, 999, 1000//target_steps)][::-1]    
+        self.uniform_steps = [i for i in range(0, 999, 1000//self.target_steps)][::-1]    
         self.adjust = True if agent1 is not None else False    
 
         self.sample_size = 256
@@ -42,6 +43,7 @@ class DiffusionEnv(gym.Env):
         })
     
         # Model
+        self.last_T = 999
         self.runner = runner
         self.target_steps = target_steps
         self.final_threshold = 0.9
@@ -59,26 +61,6 @@ class DiffusionEnv(gym.Env):
         self.current_image_idx = 0
         self.sample_size = config.data.image_size
         self.batch_size = config.sampling.batch_size
-
-
-        
-        # Dataloader needs iterator!!!
-        self.data_iter = iter(self.val_loader)
-        self.GT_image, self.classes = next(self.data_iter)
-
-        # noise
-        self.current_noise_image, y_0 = self.runner.sample_init(
-            self.GT_image,
-            self.sigma_0,
-            self.config,
-            self.deg,
-            self.H_funcs,
-            self.model,
-            self.idx_so_far,
-            self.cls_fn,
-            self.classes,
-        )
-        self.y_0 = y_0
 
         # ddim sequence
         skip = self.runner.num_timesteps // self.runner.args.timesteps
@@ -115,7 +97,6 @@ class DiffusionEnv(gym.Env):
         self.episode_init = True
         self.state = None
         # pdb.set_trace()
-        self.ddim_current_image = self.current_noise_image.clone()
 
     def seed(self, seed=None):
         # self.np_random, seed = seeding.np_random(seed)
@@ -131,42 +112,111 @@ class DiffusionEnv(gym.Env):
         self.episode_init = True
         if seed is not None:
             self.seed(seed)
-        
-        
+
+        # Reset counter, sequence
         self.current_step_num = 0
         self.time_step_sequence = []
         self.action_sequence = []
-        self.data_idx = random.randint(0, len(self.DM.dataset)-1)
+
+        # Load Image
+        self.data_iter = iter(self.val_loader)
+        self.GT_image, self.classes = next(self.data_iter)
+
+        # noise and low level image y_0, 
+        self.noise_image, y_0 = self.runner.sample_init(
+            self.GT_image,
+            self.sigma_0,
+            self.config,
+            self.deg,
+            self.H_funcs,
+            self.model,
+            self.idx_so_far,
+            self.cls_fn,
+            self.classes,
+        )
+        self.y_0 = y_0
+
+        # Initialization, extract degradation information from y_0 sigma 0, and H_func
+        self.state = initialize_generalized_steps(
+                self.noise_image.to("cuda"),
+                self.last_T,
+                self.runner.betas,
+                self.H_funcs,
+                self.y_0,
+                self.sigma_0,
+            )
+        """
+        state = {
+                    "x": x, (mapping to y_0 space)
+                    "b": b (beta)
+                    "Sigma": Sigma,
+                    "Sig_inv_U_t_y": Sig_inv_U_t_y,
+                    "U_t_y": U_t_y,
+                    "singulars": singulars,
+                    "large_singulars_index": large_singulars_index,
+                }
+        """
+
+        # Prepare Data (x0_t not sure)
+        self.x = self.noise_image
+        self.x0_t = self.state['x']
 
 
+        ddim_x = self.x.clone()
+        # t of ddim is set
+        self.ddim_state = initialize_generalized_steps(
+            ddim_x.to("cuda"),
+            self.ddim_seq[0],
+            self.runner.betas,
+            self.H_funcs,
+            self.y_0,
+            self.sigma_0,
+        )
+        ddim_x0_t = self.ddim_state['x']
 
-        if self.current_image_idx < self.valdata_len-1:
-            # print("Current image index: ", self.current_image_idx)
-            # print("Val data length: ", self.valdata_len)
-            self.current_image_idx += 1
-
-            self.current_step_num = 0
-            self.time_step_sequence = []
-            self.action_sequence = []
-
-            # Change to next image
-            self._load_next_image()
-        else:
-            # print("All images are used. Resetting to the first image.")
-            self.current_image_idx = 0
-            self.data_iter._reset(self.val_loader)
-
+        # Precoputing DDRM uniform seq
+        with torch.no_grad():
+            for i in range(self.target_steps):
+                ddim_t = torch.tensor(self.uniform_steps[i])
+                ddim_x0_t, ddim_at, ddim_et = denoise_single_step(self.ddim_state, self.model, ddim_t, self.cls_fn, self.classes)
+                if i != 0:
+                    self.ddim_state['x'] = denoise_guided_addnoise(self.ddim_state, ddim_t, ddim_at, ddim_et, ddim_x0_t, self.H_funcs, self.sigma_0, self.runner.args)
+        orig = inverse_data_transform(self.config, self.GT_image).to(self.runner.device)
+        ddim_x = inverse_data_transform(self.config, ddim_x0_t).to(self.runner.device)
+        ddim_mse = torch.mean((ddim_x - orig) ** 2)
+        self.ddim_psnr = 10 * torch.log10(1 / ddim_mse).item()
+        self.ddim_ssim = structural_similarity(ddim_x.cpu().numpy(), orig.cpu().numpy(), win_size=21, channel_axis=0, data_range=1.0)
         observation = {
-            # "lower_dim_image": (self.current_noise_image).reshape(self.batch_size, 1).cpu().numpy(),
-            "image": torch.tensor(self.current_noise_image).squeeze(0).cpu().numpy(),
-            "value": np.array([999]),
+            "image": torch.tensor(self.x0_t).squeeze(0).cpu().numpy(),
+            "value": np.array([self.last_T]),
         }
 
+        # TODO START HERE
+        if self.adjust: # Second subtask
+            action, _state = self.agent1.predict(observation, deterministic=True)
+            self.action_sequence.append(action.item())
+
+            start_t = 50 * (1+action) - 1
+            t = torch.tensor(int(max(0, min(start_t, 999))))
+            self.interval = int(t / (self.target_steps - 1)) 
+            self.state['x'] = denoise_guided_addnoise(self.state, self.at, self.et, self.x0_t, self.H_funcs, self.sigma_0, self.runner.args)
+            # Next round
+            self.t = t
+            self.x0_t, self.at, self.et = denoise_single_step(self.state, self.model, self.t, self.cls_fn, self.classes)
+            self.time_step_sequence.append(t.item())
+            observation = {
+                    "image": self.x0_t.cpu(),
+                    "value": np.array([t])
+                }
+            self.current_step_num += 1
+
+        torch.cuda.empty_cache()  # Clear GPU cache
+            
         return observation, {}
 
     def _load_next_image(self):
         self.GT_image, self.classes = next(self.data_iter)
-        self.current_noise_image, self.y_0 = self.runner.sample_init(
+        self.noise, self.y_0 = self.runner.sample_init(
             self.GT_image,
             self.sigma_0,
             self.config,
@@ -181,100 +231,79 @@ class DiffusionEnv(gym.Env):
 
     def step(self, action):
         truncate = self.current_step_num >= self.max_steps
-        # pdb.set_trace()
-        # RL Method
-        # Denoise current image at time t
-        # Q: how to design the last T
-        interval = self.interval
-        # print("interval = ", interval)
-        if self.episode_init:
-            # Randomly pick last T in a certain range
-            last_T = torch.randint(low=800, high=1000, size=(1,)).item()
-            self.state = initialize_generalized_steps(
-                self.current_noise_image.to("cuda"),
-                last_T,
-                self.runner.betas,
-                self.H_funcs,
-                self.y_0,
-                self.sigma_0,
-            )
-            self.episode_init = False
-            self.t = last_T
-            # print("type of self.t: ", type(self.t))
+        # RL
+        with torch.no_grad():
+            if self.adjust == False:
+                start_t = 50 * (1+action) - 1
+                next_t = torch.tensor(int(max(0, min(start_t, 999))))
+                self.interval = int(next_t / (self.target_steps - 1)) 
+                self.state['x'] = denoise_guided_addnoise(self.state, self.at, self.et, self.x0_t, self.H_funcs, self.sigma_0, self.runner.args)
+                self.action_sequence.append(action.item())
+            else: # Second subtask
+                next_t = self.t - self.interval - self.interval * action
+                next_t = torch.tensor(int(max(0, min(next_t, 999))))
+                self.interval = int(next_t / (self.target_steps - self.current_step_num - 1)) if (self.target_steps - self.current_step_num - 1) != 0 else self.interval
+                self.state['x'] = denoise_guided_addnoise(self.state, self.at, self.et, self.x0_t, self.H_funcs, self.sigma_0, self.runner.args)
+                self.action_sequence.append(action.item())
 
-            # t of ddim is set
-            self.ddim_state = initialize_generalized_steps(
-                self.ddim_current_image.to("cuda"),
-                self.ddim_seq[0],
-                self.runner.betas,
-                self.H_funcs,
-                self.y_0,
-                self.sigma_0,
-            )
         
-        # add current t and current action
-        self._update_sequences(self.t, action)
-
-        # TODO: Need to redesign
-        # generate next t based on current action
-        # print("type of self.t: ", type(self.t)) = int
-        # print("type of interval: ", type(interval)) = int
-        # print("type of action: ", type(action)) = ndarray
-        next_t = self._calculate_time_step(action, interval)
-
-
-        self.current_noise_image = self._perform_denoising_single_step(
-            self.state, self.t, next_t
-        )
-
-        # DDRM
-        ddim_t = self.ddim_seq[self.current_step_num]
-        # print("ddim_t = ", ddim_t)
-        self.ddim_current_image = self._perform_denoising_single_step(
-            self.ddim_state,
-            t=ddim_t,
-            next_t=self.ddim_seq_next[self.current_step_num],
-        )
-        # Check if the episode is done
-        done = self.current_step_num == self.target_steps - 1
-        self.current_step_num += 1
-
-        # Calculate reward
-        # TODO: Need to redesign
-        reward, ssim, ddim_ssim = self.calculate_reward(done) 
-        info = self._create_info_dict(ddim_t, self.t, reward, ssim, ddim_ssim)
-        # lower_dim_image = self.current_noise_image.reshape(self.batch_size, -1)
-        # observation = {"lower_dim_image": self.current_noise_image, "value": self.t}
-        observation = {"image": self.current_noise_image.squeeze(0), "value": self.t}
-
-        # Updatee RL t
         self.t = next_t
+        self.x0_t, self.at, self.et = denoise_single_step(self.state, self.model, self.t, self.cls_fn, self.classes)
+        self.time_step_sequence.append(self.t.item())
 
+        # Run remaining steps via uniform policy
+        self.uniform_x0_t = self.x0_t.clone()
+        uniform_at = self.at.clone()
+        uniform_et = self.et.clone()
+        self.uniform_state = self.state.clone()
+
+        for i in range(self.target_steps - self.current_step_num - 1): 
+            uniform_t = torch.tensor(int(self.t - self.interval - self.interval * i))
+            uniform_t = torch.tensor(max(0, min(uniform_t, 999)))
+            self.uniform_state['x'] = denoise_guided_addnoise(self.state,  uniform_at, uniform_et, self.uniform_x0_t, self.H_funcs, self.sigma_0, self.runner.args)
+            self.uniform_x0_t, uniform_at, uniform_et = denoise_single_step(self.uniform_state, self.model, uniform_t, self.cls_fn, self.classes)
+
+
+        # Finish the episode if denoising is done
+        done = (self.current_step_num == self.target_steps - 1) or not self.adjust
+        reward, ssim, psnr, ddim_ssim, ddim_psnr = self.calculate_reward(done)
+        info = {
+            'ddim_t': self.uniform_steps[self.current_step_num],
+            't': self.t,
+            'reward': reward,
+            'ssim': ssim,
+            'psnr': psnr,
+            'ddim_ssim': ddim_ssim,
+            'ddim_psnr': ddim_psnr,
+            'time_step_sequence': self.time_step_sequence,
+            'action_sequence': self.action_sequence,
+        }
+
+        observation = {
+            "image":  self.current_noise_image.cpu(),  
+            "value": np.array([self.t])
+        }
+        self.current_step_num += 1
+        torch.cuda.empty_cache()
         return observation, reward, done, truncate, info
 
-    def _calculate_time_step(self, action, interval):
-        action = action.item()
-        # print("type: ", type(self.ddim_seq[self.current_step_num]))
-        # print("action = ", action)
-        # print("current_step_num = ", self.current_step_num)
-        # print("ddim_seq = ", self.ddim_seq)
-        # print("self.ddim_seq[self.current_step_num] = ", self.ddim_seq[self.current_step_num])
-        print("interval = ", interval)
-        print("action = ", action)
-        t = int(torch.round(torch.tensor(self.ddim_seq[self.current_step_num] - interval * action)))
-        # t = int(torch.round(torch.tensor(self.t - interval * action)))
-        print("t = ", t)
-        final_t = max(0, min(t, 999))
-        print("final_t = ", final_t)
-        return torch.tensor(final_t)
+    # def _calculate_time_step_first(self, action):
+    #     start_t = 50 * (1 + action) - 1
+    #     t = torch.tensor(int(max(0, min(start_t, self.last_T))))
+    #     return t
 
-    def _update_sequences(self, t, action):
-        self.time_step_sequence.append(t.item() if type(t) == torch.Tensor else t)
-        self.action_sequence.append(action.item())
+    # def _calculate_time_step_second(self, action):
+    #     t = self.t - self.interval - self.interval * action
+    #     t = torch.tensor(int(max(0, min(t, self.last_T))))
+    #     return t
+
+    # def _update_sequences(self, t, action):
+    #     self.time_step_sequence.append(t.item() if type(t) == torch.Tensor else t)
+    #     self.action_sequence.append(action.item())
 
     def _perform_denoising_single_step(self, state, t, next_t):
         with torch.no_grad():
-            xs, x0_preds = denoise_single_step(
+            x0_t, xt_next = denoise_single_step(
                 state,
                 self.model,
                 t,
@@ -288,8 +317,9 @@ class DiffusionEnv(gym.Env):
                 cls_fn=self.cls_fn,
                 classes=self.classes,
             )
-            x = torch.stack([inverse_data_transform(self.config, y) for y in xs])
-        return x
+            # x = torch.stack([inverse_data_transform(self.config, y) for y in xs])
+            # x = inverse_data_transform(self.config, x0_t)
+        return x0_t
 
     def _create_info_dict(self, ddim_t, t, reward, ssim, ddim_ssim):
         return {
@@ -304,31 +334,32 @@ class DiffusionEnv(gym.Env):
 
     def calculate_reward(self, done):
         reward = 0
-        # similarity = torch.nn.functional.mse_loss(self.current_image, self.GT_image)
-        # ddim_similarity = torch.nn.functional.mse_loss(self.ddim_current_image, self.GT_image)
-        ssim = structural_similarity(
-            ((self.current_noise_image[0] + 1.0) / 2.0).cpu().numpy(),
-            ((self.GT_image[0] + 1.0) / 2.0).cpu().numpy(),
-            multichannel=True,
-            channel_axis=0,
-            data_range=1,
-        )
-        ddim_ssim = structural_similarity(
-            ((self.ddim_current_image[0] + 1.0) / 2.0).cpu().numpy(),
-            ((self.GT_image[0] + 1.0) / 2.0).cpu().numpy(),
-            multichannel=True,
-            channel_axis=0,
-            data_range=1,
-        )
-        # Intermediate reward
-        if ssim > ddim_ssim:
-            reward += 1 / self.target_steps
-        # Sparse reward (SSIM)
-        if done and ssim > self.final_threshold:
-            reward += 1
+        orig = inverse_data_transform(self.config, self.GT_image).to(self.runner.device)
+        if done and self.adjust:
+            x = inverse_data_transform(self.config, self.x0_t).to(self.runner.device)
+        else:
+            x = inverse_data_transform(self.config, self.uniform_x0_t).to(self.runner.device)
+        mse = torch.mean((x - orig) ** 2)
+        psnr = 10 * torch.log10(1 / mse).item()
+        ssim = structural_similarity(x.cpu().numpy(), orig.cpu().numpy(), win_size=21, channel_axis=0, data_range=1.0)
+        
+        # Intermediate reward (Percentage of temporary improvement)
+        if not done and psnr > self.ddim_psnr and ssim > self.ddim_ssim:
+            reward += 0.5/self.target_steps*psnr/self.ddim_psnr 
+            reward += 0.5/self.target_steps*ssim/self.ddim_ssim
+        
+        # Sparse reward (Percentage of final improvement)
+        if done and psnr > self.ddim_psnr and ssim > self.ddim_ssim:
+            reward += 0.5*psnr/self.ddim_psnr
+            reward += 0.5*ssim/self.ddim_ssim
 
-        return reward, ssim, ddim_ssim
+
+        return reward, ssim, psnr, self.ddim_ssim, self.ddim_psnr
 
     def render(self, mode="human", close=False):
         # This could visualize the current state if necessary
         pass
+
+    def set_adjust(self, adjust):
+        self.adjust = adjust
+        print(f"Set adjust to {adjust}")
